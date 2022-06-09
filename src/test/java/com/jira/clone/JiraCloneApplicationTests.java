f9c2845e648a00bca7368251075fdb0b546a2377
Patil Sujith <sujith.patil@scania.com>
2022-06-09 10:32:23 +0200

    first commit

---
+++ src/test/java/com/jira/clone/JiraCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jira.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
